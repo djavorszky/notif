05526f1707d75d1ba2ace0f03edde1cd90121b97
djavorszky <daniel.javorszky@liferay.com>
2017-01-31 17:37:20 +0100

    Refactor to use channels

--- main.go
+++ main.go
@@ -16,38 +16,25 @@ import (
 // used primarily, except if the method specifies otherwise.
 var Dest = "http://localhost:80/message"
 
-// Snd JSONifies the message, then sends it as a POST request to the DefaultDest.
-func Snd(msg Fireable) error {
-	return SndLoc(msg, Dest)
+// New returns a channel through which it awaits messages to be sent to the address
+func New(ID int, address string) chan Y {
+	ch := make(chan Y)
+
+	go watch(ch, ID, address)
+
+	return ch
 }
 
-// SndLoc JSONifies the message, then sends it as a POST request to the specified destination.
-func SndLoc(msg Fireable, dest string) error {
-	jMsg, err := jsonify(msg)
-	if err != nil {
-		return err
-	}
-
-	statusCode, err := sendReq(Dest, jMsg)
-	if err != nil {
-		return err
-	}
-
-	if statusCode != http.StatusOK {
-		return fmt.Errorf("Got non-200 response: %d", statusCode)
-	}
-
-	log.Println("Msg sent successfully")
-	return nil
+// Y should be used to send messages through the channel to notif.
+type Y struct {
+	StatusCode int
+	Msg        string
 }
 
 // Msg is the simplest type of message that can be sent. It has an ID field which
 // can be used to identify the message if it belongs to a conversation, a StatusID
 // which can correspond to statuses, similar to that of the HTTP response codes,
 // and a Message that can contain any text, aimed to have a human readable message.
-//
-// It also has a seperate Dest field where a custom destination can be specified.
-// If empty, DefaultDest will be used.
 type Msg struct {
 	ID, StatusID int
 	Message      string
@@ -56,6 +43,41 @@ type Msg struct {
 // Fireable is an empty interface. This way, custom structs can also be used. There
 // is no restriction on what can be applied here, as long as it's a struct.
 type Fireable interface{}
+
+func watch(ch chan Y, ID int, address string) {
+	for y := range ch {
+		m := Msg{ID, y.StatusCode, y.Msg}
+
+		err := sndLoc(m, address)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+// snd JSONifies the message, then sends it as a POST request to the DefaultDest.
+func snd(msg Fireable) error {
+	return sndLoc(msg, Dest)
+}
+
+// sndLoc JSONifies the message, then sends it as a POST request to the specified destination.
+func sndLoc(msg Fireable, dest string) error {
+	jMsg, err := jsonify(msg)
+	if err != nil {
+		return err
+	}
+
+	statusCode, err := sendReq(dest, jMsg)
+	if err != nil {
+		return err
+	}
+
+	if statusCode != http.StatusOK {
+		return fmt.Errorf("Got non-200 response: %d", statusCode)
+	}
+
+	return nil
+}
 
 func sendReq(dest string, msg []byte) (int, error) {
 	req, err := http.NewRequest(http.MethodPost, dest, bytes.NewBuffer(msg))
@@ -66,7 +88,6 @@ func sendReq(dest string, msg []byte) (int, error) {
 
 	client := &http.Client{}
 
-	log.Println("Sending msg")
 	resp, err := client.Do(req)
 	if err != nil {
 		return 0, err
